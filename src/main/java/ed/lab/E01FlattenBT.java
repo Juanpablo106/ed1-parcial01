4b92b0f3210437cd54fc20d8ad58523453fd198a
Juan Pablo <juanpaestra8@gmail.com>
2025-03-25 07:30:00 -0600

    Parcial I - Resuelto - Juan Pablo Estrada Rodríguez 1056124

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,43 @@
 package ed.lab;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class E01FlattenBT {
 
-    public void flatten(TreeNode<Integer> root) {
+    public void flatten(TreeNode<Integer> root)
+    {
+        LinkedList<Object> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
 
+        if (root == null)
+        {
+            return;
+        }
+
+        stack.push(root);
+
+        while(!stack.isEmpty())
+        {
+            TreeNode node = stack.pop();
+            result.add(node.value);
+
+            if (node.right != null)
+            {
+                stack.push(node.right);
+            }
+
+            if (node.left != null)
+            {
+                stack.push(node.left);
+            }
+
+            node.left = null;
+
+            if (!stack.isEmpty())
+            {
+                node.right = stack.peek();
+            }
+        }
     }
-
 }
